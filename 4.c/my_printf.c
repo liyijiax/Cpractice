63e10222c5655a2e0c31473ef7d8166fed4c886f
lyj <1497333171@qq.com>
2019-07-29 21:10:54 +0800

    update

--- 4.c/my_printf.c
+++ 4.c/my_printf.c
@@ -10,7 +10,7 @@
 #include<stdio.h>
 #include <stdlib.h>
 
-int my_printf(const chr *str, ...) { // 如果不用const不可以，因为不应该被修改，
+int my_printf(const chr *str, ...) { // 如果不用const不可以，因为不应该被修改，字面量的值是不能改变的，C语言也是吧字面量相同的值放在一个地址
     for (int i = 0; str[i]; i++) // 等价与 i < len 
 }
 
