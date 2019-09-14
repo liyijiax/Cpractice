33b1b9835465668e38280cf8d180c7c7f1a95ce5
lyj_win <1497333171@qq.com>
2019-09-14 20:30:03 +0800

    add

--- 6ch/2.log.cpp
+++ 6ch/2.log.cpp
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+#define COLOR(a, b) "\033[40;" #b "m" a "\033[0m"
+
+#define GREEN(a) COLOR(a, 32)
+#define BLUE(a) COLOR(a, 33)
+
 #ifdef DEBUG
 #define log(frm, args...) { \
-	printf("[%s : %d]", __func__, __LINE__); \
+	printf("\033[46;31m[%s : %d]\033[0m", __func__, __LINE__); \
 	printf(frm, ##args); \
 	printf("\n");\
 }
@@ -10,14 +15,20 @@
 #define log(frm, args...)
 #endif
 
-#define contact(a, b) a##b
+//#define contact(a, b) a##b
+
+#define __contact(a, b) a##b
+#define contact(a, b) __contact(a, b)
 
 int main() {
 	int a = 7;
 	int abcdef = 19;
-	log("%d", a);
+	log(GREEN("%d"), a);
 	log("hello world");
 	contact(abc, def) = 21;
+	log("%d", abcdef);
+	//contact(GREEN("hello"), BLUE("world"));
+	contact(contact(a, contact(b, c)), contact(d, ef)) = 33;
 	log("%d", abcdef);
 	return 0;
 }
